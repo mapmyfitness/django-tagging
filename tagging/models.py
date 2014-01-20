ff128eedc6d70e3939cb246f6e6262fce204800a
Dave McLain <dave.mclain@mapmyfitnessinc.com>
2014-01-20 16:29:45 -0600

    CORE-472 apply patch from apply_patches

--- tagging/models.py
+++ tagging/models.py
@@ -47,7 +47,8 @@ class TagManager(models.Manager):
         for tag_name in updated_tag_names:
             if tag_name not in current_tag_names:
                 tag, created = self.get_or_create(name=tag_name)
-                TaggedItem._default_manager.create(tag=tag, object=obj)
+                TaggedItem._default_manager.get_or_create(tag=tag,
+                                        content_type=ctype, object_id=obj.pk)
 
     def add_tag(self, obj, tag_name):
         """
